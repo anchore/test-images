ca4984012fa285e76c8fbf125468750be9fdc2c5
Christopher Angelo Phillips <christopher.phillips@anchore.com>
2021-10-21 16:41:07 -0400

    update dumb application to be vulnerable
    Signed-off-by: Christopher Angelo Phillips <christopher.phillips@anchore.com>

---
+++ containers/golang/go.mod
@@ -0,0 +1,5 @@
+module github.com/anchore/testgolang
+
+go 1.13
+
+require github.com/in-toto/in-toto-golang v0.2.0
---
+++ containers/golang/main.go
@@ -0,0 +1,12 @@
+package main
+
+import (
+	"fmt"
+
+	toto "github.com/in-toto/in-toto-golang/in_toto"
+)
+
+func main() {
+	t := toto.Metablock{}
+	fmt.Print(t)
+}
