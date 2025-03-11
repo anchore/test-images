4c3c23c78a955de1e99794f5ef53e372d4df469f
Ben Beauregard <ben.beauregard@anchore.com>
2025-03-11 16:55:59 -0400

    feat(multiarch): add manifest list with fake windows image
    Adds a manifest list with a linux and windows AMD64 image. The windows image is
    really an alpine image that's had its metadata updated to look like a windows
    image.
    
    Signed-off-by: Ben Beauregard <ben.beauregard@anchore.com>

--- scripts/check_makefiles.sh
+++ scripts/check_makefiles.sh
@@ -2,16 +2,22 @@
 
 # This list of exclusions is here to allow specific Makefiles to not be linked to the ContainerMakefile
 # These makefiles are expected to be doing unique things, such as building multi-arch images
-makefile_link_exclusions=('containers/alpine-multiarch-no-amd64')
+makefile_link_exclusions=('containers/alpine-multiarch-no-amd64' 'containers/alpine-multiarch-amd64-windows-linux')
 
 for makefile in $(find containers -name Makefile); do
     echo -n "Verifying Makefile link: $makefile";
     if [[ $(readlink $makefile) != "../../ContainerMakefile" ]]; then
-        if [[ ${makefile} == *${makefile_link_exclusions}* ]]; then
-            echo "	[SKIPPED]";
-            continue;
+        exclude=false
+        for i in "${makefile_link_exclusions[@]}"; do
+            if [[ ${makefile} == *$i* ]]; then
+                echo "	[SKIPPED]";
+                exclude=true
+            fi;
+        done
+        if [[ $exclude == true ]]; then
+            continue
         fi;
-        echo "Path is not linked to ../../ContainerMakefile -> $${makefile}" && exit 1;
+        echo "Path is not linked to ../../ContainerMakefile -> ${makefile}" && exit 1;
     fi;
     echo "	[OK]";
 done
